ac3f640c20049389ccd5e8922f3a96c266e57652
FrancoPrandi <francoprandi55@gmail.com>
2023-11-21 11:12:34 -0300

    feat: login&register new

---
+++ src/app/forgot-password/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ForgotPassword from "@/components/ForgotPassword";
+
+function ForgottenPassword() {
+  return (
+    <>
+      <ForgotPassword />
+    </>
+  );
+}
+
+export default ForgottenPassword;
