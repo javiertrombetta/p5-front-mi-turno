cfd9fed15a26c36bd6cb9417e06e311b9dfed267
dadario23 <dandrada23@gmail.com>
2023-11-23 14:23:22 -0300

    create hook/useAxios

--- src/components/SelectForm.jsx
+++ src/components/SelectForm.jsx
@@ -1,17 +1,32 @@
-"use client";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import useAxios from "@/hooks/useAxios";
+import { useState, useEffect } from "react";
 
 export default function BasicSelect() {
-  const [sucursal, setSucursal] = React.useState("");
+  const [branches, setBranches] = useState([]);
+  const [sucursal, setSucursal] = useState("");
+
+  const apiUrl = "http://localhost:5000/branches/allBranches";
+  const { data, loading, error } = useAxios(apiUrl);
+
+  useEffect(() => {
+    if (data) {
+      setBranches(data);
+    }
+  }, [data]);
 
   const handleChange = (event) => {
     setSucursal(event.target.value);
   };
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <Box sx={{ minWidth: 120 }}>
@@ -24,9 +39,11 @@ export default function BasicSelect() {
           label="Sucursal"
           onChange={handleChange}
         >
-          <MenuItem value="sucursal1">sucursal 1</MenuItem>
-          <MenuItem value="sucursal2">sucursal 2</MenuItem>
-          <MenuItem value="sucursal3">sucursal 3</MenuItem>
+          {branches.map((branch) => (
+            <MenuItem key={branch.id} value={branch.name}>
+              {branch.name}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </Box>
