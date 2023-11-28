d329a39dd0e86d16022a0f61f85f93ac041a8b55
Valentinguardia <valentin.guardia@outlook.com>
2023-11-27 21:05:21 -0300

    feat: login

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,66 @@
+// import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
+// import Navbar from "@/commons/Navbar";
+// import Footer from "@/commons/Footer";
+// import theme from "../styles/LightThemeMUI";
+// //import Provider from "./provider";
+// import { store } from "../hooks/store";
+// import "@/app/ui/globals.css";
+// import { inter } from "@/app/ui/fonts";
+// //import { Provider } from "react-redux";
+
+// export const metadata = {
+//   title: "Mi turno web",
+//   description: "P5 | Proyecto de Mi turno web",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <MUIThemeProvider theme={theme}>
+//       <html lang="en">
+//         <body>
+//           <Navbar />
+//           <div style={{ marginTop: "3em", minHeight: "calc(100vh - 128px)" }}>
+//             {children}
+//           </div>
+//           <Footer />
+//         </body>
+//       </html>
+//     </MUIThemeProvider>
+//   );
+// }
+"use client";
 import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
 import Navbar from "@/commons/Navbar";
 import Footer from "@/commons/Footer";
 import theme from "../styles/LightThemeMUI";
-//import Provider from "./provider";
 import { store } from "../hooks/store";
 import "@/app/ui/globals.css";
 import { inter } from "@/app/ui/fonts";
-//import { Provider } from "react-redux";
+import RoutesProtection from "./routesProtection";
+import { Provider } from "react-redux";
 
-export const metadata = {
-  title: "Mi turno web",
-  description: "P5 | Proyecto de Mi turno web",
-};
+// export const metadata = {
+//   title: "Mi turno web",
+//   description: "P5 | Proyecto de Mi turno web",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <MUIThemeProvider theme={theme}>
       <html lang="en">
-        <body>
-          <Navbar />
-          <div style={{ marginTop: "3em", minHeight: "calc(100vh - 128px)" }}>
-            {children}
-          </div>
-          <Footer />
-        </body>
+        <Provider store={store}>
+          <body>
+            <Navbar />
+            <RoutesProtection>
+              <div
+                style={{ marginTop: "3em", minHeight: "calc(100vh - 128px)" }}
+              >
+                {children}
+              </div>
+            </RoutesProtection>
+            <Footer />
+          </body>
+        </Provider>
       </html>
     </MUIThemeProvider>
   );
