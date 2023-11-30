0b9f40f6d685b6903c8472f97e43243dbfd5a86d
dadario23 <dandrada23@gmail.com>
2023-11-30 20:18:58 -0300

    create getReservationData

--- src/services/dataReservation.js
+++ src/services/dataReservation.js
@@ -1,20 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:3000';
+const API_URL = "http://localhost:3000";
 
 export const createReservation = async (reservationData) => {
   try {
-    const response = await axios.post(`${API_URL}/reservations`, reservationData, { withCredentials: true });
+    const response = await axios.post(
+      `${API_URL}/reservations`,
+      reservationData,
+      { withCredentials: true }
+    );
     return response.data;
   } catch (error) {
-    console.error('Error al crear la reserva:', error.response?.data?.message || error.message);
+    console.error(
+      "Error al crear la reserva:",
+      error.response?.data?.message || error.message
+    );
     throw error;
   }
 };
 
 export const getReservationsData = async () => {
   try {
-    const response = await axios.get(`${API_URL}/ruta-a-tu-api-de-reservas`, {
+    const response = await axios.get(`${API_URL}/reservations/me`, {
       withCredentials: true, // si estás usando cookies
     });
     return response.data;
