264f04b6775375ffd91b0f4a5e89acf56f8fe7ac
javiertrombetta <jitrombetta@hotmail.com>
2023-12-06 04:48:01 -0300

    feat: add dashboard operation

--- src/services/dataMetrics.js
+++ src/services/dataMetrics.js
@@ -2,13 +2,16 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000";
 
-export const getMetricsData = async (branchId = null) => {
+export const getMetricsData = async (branchId = null, selectedDate = null) => {
   try {
     let url = `${API_URL}/reservations/dashboard`;
+    const params = {};
     if (branchId) {
-      url += `/${branchId}`;
-    }
-    const response = await axios.get(url, { withCredentials: true });
+      url += `/${branchId}`;    }
+    if (selectedDate) {
+      params.date = selectedDate.toISOString().split('T')[0]; // Formato YYYY-MM-DD
+    }    
+    const response = await axios.get(url, { params, withCredentials: true });
     return response.data.metrics;
   } catch (error) {
     console.error("Error al obtener métricas:", error.message);
