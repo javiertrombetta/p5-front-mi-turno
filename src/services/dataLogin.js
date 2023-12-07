9799386ad67f44ca6f3c720fda4e15662f3fbc80
javiertrombetta <jitrombetta@hotmail.com>
2023-12-07 06:32:34 -0300

    bug: login loop

--- src/services/dataLogin.js
+++ src/services/dataLogin.js
@@ -2,19 +2,15 @@ import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const checkAuth = async () => {
+export const checkAuth = async (user, router, dispatch) => {
   try {
     const response = await axios.get(`${API_URL}/users/me`, {
       withCredentials: true,
     });
-    return response.data;
+    const userData = response.data;
+    return userData;
   } catch (error) {
-    if (error.response && error.response.status === 401) {
-      return null;
-    } else {
-      console.error("Error al verificar la autenticación:", error);
-      throw error;
-    }
+    console.error("Error al verificar la autenticación:", error);
   }
 };
 
@@ -23,15 +19,11 @@ export const loginUser = async (email, password) => {
     const response = await axios.post(
       `${API_URL}/users/login`,
       { email, password },
-      { withCredentials: true,
-        headers: { "Content-Type": "application/json" },
-        withCredentials: true,
-        credentials: "include",
-      }
-      
+      { withCredentials: true }
     );
     return response.data.payload;
   } catch (error) { 
     throw error;
   }
 };
+
