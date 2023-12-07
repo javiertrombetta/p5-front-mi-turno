3a9c82d8189ef7802efe542f78cc4e38ad058534
javiertrombetta <jitrombetta@hotmail.com>
2023-12-07 06:34:20 -0300

    bug: rollback

--- src/app/routesProtection.js
+++ src/app/routesProtection.js
@@ -1,49 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { checkAuth } from '@/services/dataLogin';
-import { loginSuccess, logoutSuccess } from '@/hooks/slices/authSlice';
-import Navbar from '@/commons/Navbar';
-import Footer from '@/commons/Footer';
-import Alert from '@/commons/Alert';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { checkAuth } from "@/services/dataLogin";
+import { loginSuccess, logoutSuccess, isLoggedIn } from "@/hooks/slices/authSlice";
+import Navbar from "@/commons/Navbar";
+import Footer from "@/commons/Footer";
+import Alert from "@/commons/Alert";
 import MailButton from '@/components/MailButton';
-import Loader from '@/components/Loader';
+import CircularProgress from "@mui/material/CircularProgress";
 
 const RoutesProtection = ({ children }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
+  const { isLogged } = useSelector((state) => state.auth);
   const [isLoading, setIsLoading] = useState(true);
-  const [alert, setAlert] = useState({ open: false, message: '' });
+  const [alert, setAlert] = useState({ open: false, message: "" });
 
   useEffect(() => {
     const checkUser = async () => {
-      if (!user) {
-        const axiosUser = await checkAuth();
-        if (axiosUser) {
-          dispatch(loginSuccess({ user: axiosUser }));
-        } else {          
+      if (user) {
+        dispatch(isLoggedIn());
+      } else {
+        try {
+          const axiosUser = await checkAuth();
+          if (axiosUser) {
+            dispatch(loginSuccess({ user: axiosUser }));
+          } else {
+            dispatch(logoutSuccess());
+          }
+        } catch (error) {
+          console.error("Error en autenticación:", error);
           dispatch(logoutSuccess());
+        } finally {
+          setIsLoading(false);
         }
-        setIsLoading(false);
       }
     };
     checkUser();
-  }, [dispatch, user]);
+  }, [dispatch, user, isLogged]);
 
   if (isLoading) {
-    return <Loader />;
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
   }
 
   return (
-    <div className="root-container">
-      {user && <Navbar />}
-      <main className="main-content">{children}</main>
-      {user && <Footer />}
-      {user && user.role !== 'super' && <MailButton />}
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      {isLogged && <Navbar />}
+      <main style={{ flex: 1, marginTop: '3em' }}>
+        {children}
+      </main>
+      {isLogged && <Footer />}
+      {isLogged && user.role != 'super' && <MailButton />}
       {alert.open && (
-        <Alert
-          message={alert.message}
-          onClose={() => setAlert({ ...alert, open: false })}
-        />
+        <Alert message={alert.message} onClose={() => setAlert({ ...alert, open: false })} />
       )}
     </div>
   );
