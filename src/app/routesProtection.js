f8fa99591e24754818f01fed8f3b201db194ab6c
javiertrombetta <jitrombetta@hotmail.com>
2023-12-07 06:09:55 -0300

    bug: redux reset

--- src/app/routesProtection.js
+++ src/app/routesProtection.js
@@ -1,76 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { checkAuth } from "@/services/dataLogin";
-import { loginSuccess, logoutSuccess, isLoggedIn } from "@/hooks/slices/authSlice";
-import Navbar from "@/commons/Navbar";
-import Footer from "@/commons/Footer";
-import Alert from "@/commons/Alert";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkAuth } from '@/services/dataLogin';
+import {
+  loginSuccess,
+  logoutSuccess,
+  isLoggedIn,
+} from '@/hooks/slices/authSlice';
+import Navbar from '@/commons/Navbar';
+import Footer from '@/commons/Footer';
+import Alert from '@/commons/Alert';
 import MailButton from '@/components/MailButton';
-import CircularProgress from "@mui/material/CircularProgress";
-import { store } from '@/hooks/store';
+import Loader from '@/components/Loader';
 
 const RoutesProtection = ({ children }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
-  const { isLogged } = useSelector((state) => state.auth);
   const [isLoading, setIsLoading] = useState(true);
-  const [alert, setAlert] = useState({ open: false, message: "" });
+  const [alert, setAlert] = useState({ open: false, message: '' });
 
   useEffect(() => {
     const checkUser = async () => {
-      if (user) {
-        dispatch(isLoggedIn());
-      } else {
+      if (!user) {
         try {
           const axiosUser = await checkAuth();
           if (axiosUser) {
             dispatch(loginSuccess({ user: axiosUser }));
           } else {
             dispatch(logoutSuccess());
-            store.dispatch({ type: 'RESET_APP' });
           }
         } catch (error) {
-          console.error("Error en autenticación:", error);
+          console.error('Error en autenticación:', error);
           dispatch(logoutSuccess());
-          store.dispatch({ type: 'RESET_APP' });
+          setAlert({ open: true, message: 'Error en autenticación' });
         } finally {
           setIsLoading(false);
         }
       }
     };
     checkUser();
-  }, [dispatch, user, isLogged]);
+  }, [dispatch, user]);
 
   if (isLoading) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          position: "fixed",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0
-        }}
-      >
-        <CircularProgress />
-      </div>
-    );
+    return <Loader />;
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      {isLogged && <Navbar />}
-      <main style={{ flex: 1, marginTop: '3em' }}>
-        {children}
-      </main>
-      {isLogged && <Footer />}
-      {isLogged && user.role != 'super' && <MailButton />}
+    <div className="root-container">
+      {user && <Navbar />}
+      <main className="main-content">{children}</main>
+      {user && <Footer />}
+      {user && user.role !== 'super' && <MailButton />}
       {alert.open && (
-        <Alert message={alert.message} onClose={() => setAlert({ ...alert, open: false })} />
+        <Alert
+          message={alert.message}
+          onClose={() => setAlert({ ...alert, open: false })}
+        />
       )}
     </div>
   );
